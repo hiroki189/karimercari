3a58f32e37bf747ac916959f754c14ff7f91571b
hiroki189 <hiroki18950@icloud.com>
2019-06-23 12:15:28 +0900

    user_新規登録画面

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get 'mercaris' => 'mercaris#index'
+  get 'users' => 'users#edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
